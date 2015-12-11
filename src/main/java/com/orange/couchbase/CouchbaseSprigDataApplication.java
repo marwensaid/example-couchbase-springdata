a720d65246ddacb7ac1761a41b438562888e6a9d
marwensaid <marwensaidi1@gmail.com>
2015-12-11 10:48:02 +0100

    feat(couchbase-spring-data) add project from spring.io --MS

---
+++ src/main/java/com/orange/couchbase/CouchbaseSprigDataApplication.java
@@ -0,0 +1,12 @@
+package com.orange.couchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchbaseSprigDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouchbaseSprigDataApplication.class, args);
+    }
+}
---
+++ src/test/java/com/orange/couchbase/CouchbaseSprigDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orange.couchbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CouchbaseSprigDataApplication.class)
+public class CouchbaseSprigDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
