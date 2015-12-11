76990b25a17f74fd6141112b8cd17e82f731ba8c
marwensaid <marwensaidi1@gmail.com>
2015-12-11 11:43:45 +0100

    feat(couchbase-spring-data) implement twitterUpdateRepo --MS

--- src/main/java/com/orange/couchbase/metrics/TwitterUpdateRepository.java
+++ src/main/java/com/orange/couchbase/metrics/TwitterUpdateRepository.java
@@ -1,10 +1,16 @@
 package com.orange.couchbase.metrics;
 
+import org.springframework.data.repository.CrudRepository;
+import com.couchbase.client.protocol.views.Query;
+import java.util.Collection;
+
 /**
  * Created by msaidi on 12/11/15.
  */
-public class TwitterUpdateRepository {
-    public static void save(TwitterUpdate twitterUpdate) {
+public interface TwitterUpdateRepository extends CrudRepository<TwitterUpdate, String> {
 
-    }
+    Collection<TwitterUpdate> findByDate(Query query);
+
+    Collection<TwitterUpdate> findFollowersByDate(Query query);
+
 }
