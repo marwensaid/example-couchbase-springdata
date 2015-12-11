5f8d68233eb868afeff21aa312089f1c58356f94
marwensaid <marwensaidi1@gmail.com>
2015-12-11 11:36:34 +0100

    feat(couchbase-spring-data) implement twitterUpdate --MS

--- src/main/java/com/orange/couchbase/metrics/TwitterUpdate.java
+++ src/main/java/com/orange/couchbase/metrics/TwitterUpdate.java
@@ -4,7 +4,51 @@ package com.orange.couchbase.metrics;
  * Created by msaidi on 12/11/15.
  */
 public class TwitterUpdate {
+
+    private final String key;
+    private final long timeCreate;
+    private final String twitterAccount;
+    private final int favoritesCount;
+    private final int followersCount;
+    private final int friendsCount;
+    private final int statusesCount;
+
     public TwitterUpdate(String key, long timeCreate, String twitterAccount, int favoritesCount, int followersCount, int friendsCount, int statusesCount) {
 
+        this.key = key;
+        this.timeCreate = timeCreate;
+        this.twitterAccount = twitterAccount;
+        this.favoritesCount = favoritesCount;
+        this.followersCount = followersCount;
+        this.friendsCount = friendsCount;
+        this.statusesCount = statusesCount;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public long getTimeCreate() {
+        return timeCreate;
+    }
+
+    public String getTwitterAccount() {
+        return twitterAccount;
+    }
+
+    public int getFavoritesCount() {
+        return favoritesCount;
+    }
+
+    public int getFollowersCount() {
+        return followersCount;
+    }
+
+    public int getFriendsCount() {
+        return friendsCount;
+    }
+
+    public int getStatusesCount() {
+        return statusesCount;
     }
 }
